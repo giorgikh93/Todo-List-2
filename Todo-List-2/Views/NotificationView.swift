55bd9aa5e1c7d372442f991c8210aa6f817df0d8
giorgikh93 <giorgikhatiashvili93@yahoo.com>
2022-07-08 17:36:37 +0400

    NotificationManager

---
+++ Todo-List-2/Views/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Todo-List-2
+//
+//  Created by giorgi on 08.07.22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
